8fe2ca1acd61338d9cb73a904894d4480d7d719d
cam <cjohnson23@gwu.edu>
2022-06-22 20:04:54 -0400

    idfk

--- valid_signals_length.py
+++ valid_signals_length.py
@@ -5,6 +5,9 @@ Description: Generate csv of all patients ids, segments ids for all
              segments at least 10 min in length (75,000 samples) that
              have PLETH & ABP.         
 """
+# TODO: Error was because of issue with concatenating file name for deletion.
+#       Make runcmd verbose and watch it run for a little.
+#       Do it for a couple patients just so theres some data to base next script off of.
 import csv
 from tqdm import tqdm
 from utils import runcmd
@@ -20,17 +23,22 @@ DIR_NUM = '30/'
 
 valid_patients = []
 base_dir = 'physionet.org/files/mimic3wdb/1.0/'
-for rec in tqdm(RECORDS):
+i = 0
+for rec in RECORDS:
+    print(i)
+    i += 1
+    if i == 30:
+        break
     pid = rec[0]
     valid_segments = []
-    for sid in rec[1::]:
+    for sid in tqdm(rec[1::]):
         path = base_dir + DIR_NUM + str(pid) + f'/{sid}'
-
+        
         runcmd('wget -r -np https://' + path + '.hea')
         hea = rdheader(path)
         if ('PLETH' in hea.sig_name) & ('ABP' in hea.sig_name):
             valid_segments.append(sid)
-        runcmd('rm ' + path)
+        runcmd('rm ' + path + '.hea')
     if len(valid_segments) != 0:
         valid_segments.insert(0, pid)
         valid_patients.append(valid_segments)
