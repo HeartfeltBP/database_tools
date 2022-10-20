9062a76cde3e9c9d5523608db7a25c4bb638e59b
camhpj <82114003+camhpj@users.noreply.github.com>
2022-10-20 17:40:02 -0400

    Update get_valid_segs.py

--- scripts/get_valid_segs.py
+++ scripts/get_valid_segs.py
@@ -4,5 +4,8 @@ from database_tools.tools import DataLocator
 repo_dir = '/path/to/repository/'
 os.chdir(repo_dir)
 
-worker = DataLocator(data_dir='physionet.org/files/mimic3wdb/1.0/')
+worker = DataLocator(
+    data_dir='path/to/data/dir/',
+    mimic3_dir='physionet.org/files/mimic3wdb/1.0/',
+)
 worker.run()
