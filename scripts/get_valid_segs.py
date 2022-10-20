8559ce1d652246e54fa7ded5c505d816c6b3fe44
camhpj <82114003+camhpj@users.noreply.github.com>
2022-10-20 17:19:31 -0400

    Update get_valid_segs.py

--- scripts/get_valid_segs.py
+++ scripts/get_valid_segs.py
@@ -1,7 +1,7 @@
 import os
 from database_tools.tools import DataLocator
 
-repo_dir = '/media/cam/CAPSTONEDB/database_tools/'
+repo_dir = '/path/to/repository/'
 os.chdir(repo_dir)
 
 worker = DataLocator(data_dir='physionet.org/files/mimic3wdb/1.0/')
