2ec48657f53a972104a477223d96e1a08f4c7d8c
camhpj <cjohnson23@gwu.edu>
2022-11-10 15:12:39 -0500

    updated records handler to work with 1, 2, and 3d input

--- database_tools/tools/RecordsHandler.py
+++ database_tools/tools/RecordsHandler.py
@@ -1,17 +1,19 @@
 import glob
+import time
 import numpy as np
 import pandas as pd
 import pickle as pkl
 import tensorflow as tf
 from tqdm import tqdm
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 
 class RecordsHandler():
     def __init__(self, data_dir):
         self._data_dir = data_dir
 
-    def generate_records(self, split_strategy=(0.70, 0.15, 0.15), max_samples=10000, normalize='MinMax'):
+    def generate_records(self, labels, split_strategy=(0.70, 0.15, 0.15), max_samples=10000):
         """
         Generates TFRecords files from JSONLINES files.
 
@@ -22,73 +24,73 @@ class RecordsHandler():
         """
         print('Loading data...')
         df = self._compile_lines(self._data_dir)
-        ppg_set = np.array(df['ppg'].to_list())
-        abp_set = np.array(df['abp'].to_list())
 
-        print('Scaling data...')
-        if normalize == 'Standard':
-            ppg_scaler = StandardScaler()
-            abp_scaler = StandardScaler()
-        elif normalize == 'MinMax':
-            ppg_scaler = MinMaxScaler(feature_range=(0, 1))
-            abp_scaler = MinMaxScaler(feature_range=(0, 1))
-        ppg_set = ppg_scaler.fit_transform(ppg_set)
-        abp_set = abp_scaler.fit_transform(abp_set)
-
-        # Save scalers
-        with open(f'{self._data_dir}ppg_scaler_{normalize}.pkl', 'wb') as f:
-            pkl.dump(ppg_scaler, f)
-        with open(f'{self._data_dir}abp_scaler_{normalize}.pkl', 'wb') as f:
-            pkl.dump(abp_scaler, f)
-
-        print('Splitting data...')
-        idx = np.random.permutation(len(df))
-        i = int(len(idx) * split_strategy[0])
-        j = int(len(idx) * split_strategy[1])
-
-        idx_train = idx[0:i]
-        idx_val = idx[i:i+j]
-        idx_test = idx[i+j::]
-
-        ppg_train = ppg_set[idx_train]
-        ppg_val = ppg_set[idx_val]
-        ppg_test = ppg_set[idx_test]
-        
-        abp_train = abp_set[idx_train]
-        abp_val = abp_set[idx_val]
-        abp_test = abp_set[idx_test]
-
-        ppg_dict=dict(
-            train=ppg_train,
-            val=ppg_val,
-            test=ppg_test
+        # Determine indices for split
+        idx = [i for i in range(0, 200000)]
+        test_size = split_strategy[1] + split_strategy[2]
+        idx_train, idx_val = train_test_split(
+            idx,
+            train_size=split_strategy[0],
+            test_size=test_size
+        )
+        idx_val, idx_test = train_test_split(
+            idx_val,
+            train_size=round(split_strategy[1] / test_size, 5),
+            test_size=round(split_strategy[2] / test_size, 5)
         )
 
-        abp_dict=dict(
-            train=abp_train,
-            val=abp_val,
-            test=abp_test,
-        )
+        data_unscaled = {}
+        for l in labels:
+            if l == 'vpg':
+                temp = np.gradient(data_unscaled['ppg'], 0.1, axis=1)  # 1st derivative of ppg
+            elif l == 'apg':
+                temp = np.gradient(data_unscaled['vpg'], 0.1, axis=1)  # 2nd derivative of ppg
+            else:
+                temp = np.array(df[l].to_list())
+            data_unscaled[l] = temp
+
+        data = {}
+        scalers = {}
+        for l in labels:
+            print(f'Scaling {l}...')
+            std_scaler = StandardScaler()
+            temp = std_scaler.fit_transform(data_unscaled[l])
+
+            print(f'Splitting {l}...')
+            train = temp[idx_train]
+            val = temp[idx_val]
+            test = temp[idx_test]
+            data[l] = dict(
+                train=train,
+                val=val,
+                test=test,
+            )
+            scalers[l] = std_scaler
+
+        r = time.time_ns()
+        with open(f'{self._data_dir}scaler_{r}.pkl', 'wb') as f:
+            pkl.dump(scalers, f)
 
         print('Generating records...')
-        output_dir = self._data_dir + 'records/'
-        for split in ['train', 'val', 'test']:
+        output_dir = self._data_dir + f'records_{len(labels) - 1}d/'
+        for i, split in enumerate(['train', 'val', 'test']):
             print(f'Starting {split} split...')
-            ppg = ppg_dict[split]
-            abp = abp_dict[split]
+            split_data = {f'{k}': data[k][split] for k in data.keys()}
 
             file_number = 0
             num_samples = 0
             examples = []
-            for ppg_win, abp_win in tqdm(zip(ppg, abp), total=ppg.shape[0]):
+
+            total = len(idx) * split_strategy[i]
+            for win in tqdm(zip(*split_data.values()), total=int(total)):
                 examples.append(
                     self._full_wave_window_example(
-                        ppg=ppg_win,
-                        abp=abp_win,
+                        win=win,
+                        labels=labels,
                     )
                 )
                 num_samples += 1
-                if ((num_samples % max_samples) == 0) | (num_samples == len(ppg)):
+                if ((num_samples % max_samples) == 0) | (num_samples == total):
                     self._write_record(examples, split, file_number, output_dir)
                     file_number += 1
                     examples = []
@@ -106,11 +108,8 @@ class RecordsHandler():
         """Returns a float_list from a float list."""
         return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
-    def _full_wave_window_example(self, ppg, abp):
-        feature = {
-            'ppg' : self._float_array_feature(ppg),
-            'abp' : self._float_array_feature(abp),
-        }
+    def _full_wave_window_example(self, win, labels):
+        feature = {f'{l}': self._float_array_feature(win[i]) for i, l in enumerate(labels)}
         example = tf.train.Example(features=tf.train.Features(feature=feature))
         return example
 
@@ -120,21 +119,61 @@ class RecordsHandler():
             for tf_example in examples:
                 w.write(tf_example.SerializeToString())
 
-    def read_records(self, n_cores, AUTOTUNE):
+    def read_records(self, splits, labels, n_cores, AUTOTUNE):
         data_splits = {}
-        for split in ['train', 'val', 'test']:
-            print(f'Reading {split} split.')
-            filenames = [file for file in glob.glob(f'{self._data_dir}records/{split}/*.tfrecords')]
+        for s in splits:
+            print(f'Reading {s} split.')
+            filenames = [file for file in glob.glob(f'{self._data_dir}records_{len(labels)-1}d/{s}/*.tfrecords')]
             dataset = tf.data.TFRecordDataset(
                 filenames=filenames,
                 compression_type=None,
                 buffer_size=100000000,
                 num_parallel_reads=n_cores
             )
-            data_splits[split] = dataset.map(self._full_wave_parse_window_function, num_parallel_calls=AUTOTUNE)
+            if labels == ['ppg', 'vpg', 'apg', 'abp']:
+                data_splits[s] = dataset.map(self._full_wave_parse_window_function3, num_parallel_calls=AUTOTUNE)
+            elif labels == ['ppg', 'vpg', 'abp']:
+                data_splits[s] = dataset.map(self._full_wave_parse_window_function2, num_parallel_calls=AUTOTUNE)
+            elif labels == ['ppg', 'abp']:
+                data_splits[s] = dataset.map(self._full_wave_parse_window_function1, num_parallel_calls=AUTOTUNE)
+            else:
+                raise ValueError(f'Invalid labels: {labels}')
         return data_splits
 
-    def _full_wave_parse_window_function(self, example_proto):
+    def _full_wave_parse_window_function3(self, example_proto):
+        print('test')
+        features = tf.io.parse_single_example(
+            example_proto, 
+            features = {
+                'ppg': tf.io.FixedLenFeature([256], tf.float32),
+                'vpg': tf.io.FixedLenFeature([256], tf.float32),
+                'apg': tf.io.FixedLenFeature([256], tf.float32),
+                'abp': tf.io.FixedLenFeature([256], tf.float32),
+            }
+        )
+        ppg = tf.reshape(features['ppg'], (256, 1))
+        vpg = tf.reshape(features['vpg'], (256, 1))
+        apg = tf.reshape(features['apg'], (256, 1))
+        inputs = dict(ppg=ppg, vpg=vpg, apg=apg)
+        label = tf.reshape(features['abp'], (256, 1))
+        return inputs, label
+
+    def _full_wave_parse_window_function2(self, example_proto):
+        features = tf.io.parse_single_example(
+            example_proto, 
+            features = {
+                'ppg': tf.io.FixedLenFeature([256], tf.float32),
+                'vpg': tf.io.FixedLenFeature([256], tf.float32),
+                'abp': tf.io.FixedLenFeature([256], tf.float32),
+            }
+        )
+        ppg = tf.reshape(features['ppg'], (256, 1))
+        vpg = tf.reshape(features['vpg'], (256, 1))
+        inputs = dict(ppg=ppg, vpg=vpg)
+        label = tf.reshape(features['abp'], (256, 1))
+        return inputs, label
+
+    def _full_wave_parse_window_function1(self, example_proto):
         features = tf.io.parse_single_example(
             example_proto, 
             features = {
@@ -143,5 +182,6 @@ class RecordsHandler():
             }
         )
         ppg = tf.reshape(features['ppg'], (256, 1))
+        inputs = dict(ppg=ppg)
         label = tf.reshape(features['abp'], (256, 1))
-        return ppg, label
+        return inputs, label
--- database_tools/tools/__init__.py
+++ database_tools/tools/__init__.py
@@ -2,4 +2,3 @@ from .DataLocator import DataLocator
 from .BuildDatabase import BuildDatabase
 from .DataEvaluator import DataEvaluator
 from .RecordsHandler import RecordsHandler
-from .RecordsHandlerV2 import RecordsHandlerV2
