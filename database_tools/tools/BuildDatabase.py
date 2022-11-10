removed max per patient; fixed dx calculation; added used records logic

database_tools/tools/BuildDatabase.py
@@ -1,5 +1,7 @@
 import json
+import numpy as np
 import pandas as pd
+from glob import glob
 from tqdm import tqdm
 from database_tools.preprocessing import SignalProcessor
 
@@ -19,23 +21,26 @@ class BuildDatabase():
         self._win_len = win_len
         self._fs = fs
         self._samples_per_file = samples_per_file
-        self._samples_per_patient = samples_per_patient
         self._max_samples = max_samples
         self._data_dir = data_dir
 
     def run(self):
         valid_segs_path = self._output_dir + 'valid_segs.csv'
-        valid_segs = self._get_valid_segs(valid_segs_path)
+        used_segs_path = self._output_dir + 'used_segs.pkl'
+        valid_segs = self._get_valid_segs(valid_segs_path, used_segs_path)
 
         sample_gen = SignalProcessor(
-            files=valid_segs['url'],
-            samples_per_patient=self._samples_per_patient,
+            files=valid_segs,
+            output_dir=self._output_dir,
             win_len=self._win_len,
             fs=self._fs,
         )
 
         samples = ''
-        n_samples = 0
+        try:
+            n_samples = (np.max([int(i.split('/')[-1][7:14]) for i in glob(self._output_dir + 'mimic3/lines/' + '*.jsonlines')]) + 1) * self._samples_per_file
+        except:
+            n_samples = 0
         print('Starting sample generator...')
         for ppg, abp in tqdm(sample_gen.run(config=self._config), total=self._max_samples):
             samples += json.dumps(dict(ppg=ppg.tolist(), abp=abp.tolist())) + '\n'
@@ -54,11 +59,14 @@ class BuildDatabase():
         print('Done!')
         return
 
-    def _get_valid_segs(self, path):
-        df = pd.read_csv(path, names=['url'])
-        return df
+    def _get_valid_segs(self, valid_path, used_path):
+        df_valid = pd.read_csv(valid_path, names=['url'])
+        df_used = pd.read_csv(used_path, names=['url'])
+        all_valid = set(df_valid['url'])
+        used = set(df_used['url'])
+        valid = list(all_valid.difference(used))
+        return pd.Series(valid)
 
     def _write_to_jsonlines(self, output, outfile):
-        # print(f'Writing to {outfile}')
         with open(outfile, 'w') as f:
             f.write(output)

database_tools/tools/RecordsHandlerV2.py
@@ -23,22 +23,29 @@ class RecordsHandlerV2():
         print('Loading data...')
         df = self._compile_lines(self._data_dir)
         ppg_set = np.array(df['ppg'].to_list())
+        vpg_set = np.gradient(ppg_set, 0.1, axis=1)  # first derivative of ppg
+        # apg_set = np.gradient(vpg_set, 0.1, axis=1)  # second derivative of ppg
         abp_set = np.array(df['abp'].to_list())
 
         print('Scaling data...')
         if normalize == 'Standard':
             ppg_scaler = StandardScaler()
+            vpg_scaler = StandardScaler()
             abp_scaler = StandardScaler()
         elif normalize == 'MinMax':
             ppg_scaler = MinMaxScaler(feature_range=(0, 1))
+            vpg_scaler = MinMaxScaler(feature_range=(0, 1))
             abp_scaler = MinMaxScaler(feature_range=(0, 1))
         ppg_set = ppg_scaler.fit_transform(ppg_set)
+        vpg_set = vpg_scaler.fit_transform(vpg_set)
         abp_set = abp_scaler.fit_transform(abp_set)
 
         # Save scalers
-        with open(f'{self._data_dir}ppg_scaler_{normalize}.pkl', 'wb') as f:
+        with open(f'{self._data_dir}ppg_scalerv2_{normalize}.pkl', 'wb') as f:
             pkl.dump(ppg_scaler, f)
-        with open(f'{self._data_dir}abp_scaler_{normalize}.pkl', 'wb') as f:
+        with open(f'{self._data_dir}vpg_scalerv2_{normalize}.pkl', 'wb') as f:
+            pkl.dump(vpg_scaler, f)
+        with open(f'{self._data_dir}abp_scalerv2_{normalize}.pkl', 'wb') as f:
             pkl.dump(abp_scaler, f)
 
         print('Splitting data...')
@@ -53,18 +60,28 @@ class RecordsHandlerV2():
         ppg_train = ppg_set[idx_train]
         ppg_val = ppg_set[idx_val]
         ppg_test = ppg_set[idx_test]
-        
+
+        vpg_train = vpg_set[idx_train]
+        vpg_val = vpg_set[idx_val]
+        vpg_test = vpg_set[idx_test]
+
         abp_train = abp_set[idx_train]
         abp_val = abp_set[idx_val]
         abp_test = abp_set[idx_test]
 
-        ppg_dict=dict(
+        ppg_dict = dict(
             train=ppg_train,
             val=ppg_val,
             test=ppg_test
         )
 
-        abp_dict=dict(
+        vpg_dict = dict(
+            train=vpg_train,
+            val=vpg_val,
+            test=vpg_test,
+        )
+
+        abp_dict = dict(
             train=abp_train,
             val=abp_val,
             test=abp_test,
@@ -75,18 +92,18 @@ class RecordsHandlerV2():
         for split in ['train', 'val', 'test']:
             print(f'Starting {split} split...')
             ppg = ppg_dict[split]
+            vpg = vpg_dict[split]
             abp = abp_dict[split]
 
             file_number = 0
             num_samples = 0
             examples = []
-            for ppg_win, abp_win in tqdm(zip(ppg, abp), total=ppg.shape[0]):
-                vpg = np.gradient(ppg_win)
+            for ppg_win, vpg_win, abp_win in tqdm(zip(ppg, vpg, abp), total=ppg.shape[0]):
                 examples.append(
                     self._full_wave_window_example(
                         ppg=ppg_win,
                         abp=abp_win,
-                        vpg=vpg,
+                        vpg=vpg_win,
                     )
                 )
                 num_samples += 1