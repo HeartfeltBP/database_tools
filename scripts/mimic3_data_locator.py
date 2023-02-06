41f45f5f6016b09034888710ae5d6766df09304e
camhpj <cjohnson23@gwu.edu>
2023-02-05 19:01:41 -0500

    updated mimic3 data locator script; added vitaldb data locator script

--- scripts/mimic3_data_locator.py
+++ scripts/mimic3_data_locator.py
@@ -1,9 +1,9 @@
-from database_tools.tools import build_data_directory, DataLocator
+from database_tools.tools import build_data_directory, MimicDataLocator
 
 repo_dir = '/home/cam/Documents/database_tools/'
 data_dir = build_data_directory(repo_dir, 'mimic3')
 
-worker = DataLocator(
+worker = MimicDataLocator(
     data_dir=data_dir,
     mimic3_dir='physionet.org/files/mimic3wdb/1.0/',
 )
---
+++ scripts/vital_data_locator.py
@@ -0,0 +1,10 @@
+import vitaldb
+import pandas as pd
+from database_tools.tools import build_data_directory
+
+repo_dir = '/home/cam/Documents/database_tools/'
+data_dir = build_data_directory(repo_dir, 'vital')
+
+caseids = vitaldb.find_cases(['ART', 'PLETH'])
+df = pd.DataFrame(dict(id=caseids, url=caseids))
+df.to_csv(data_dir + 'valid_segs.csv', index=False)
